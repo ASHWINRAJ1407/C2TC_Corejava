084e67e02c777a9e63bfac0ede9773c73bc631c1
ashwi <ashwi@ASHWIN>
2025-09-22 15:30:23 +0530

    commit

---
+++ core_java123/src/core_java123/JavaJava.java
@@ -0,0 +1,11 @@
+package core_java123;
+
+public class JavaJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome java");
+
+	}
+
+}
