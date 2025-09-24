eec2ebf20bcfd9e6a39860fd0530d5aba0b81c15
ashwi <ashwi@ASHWIN>
2025-09-24 06:26:43 +0530

    commit

---
+++ core_java123/src/core_java123/IdetifiersDemo.java
@@ -0,0 +1,13 @@
+package core_java123;
+
+public class IdetifiersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Ashwin"; //use camelcase
+		System.out.println("value is : "+ studentName);
+	}
+
+}
