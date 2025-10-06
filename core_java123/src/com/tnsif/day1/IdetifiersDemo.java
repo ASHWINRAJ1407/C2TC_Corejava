07af63184da7e3eb2a2faf6ba64cebfc1de3b264
ashwi <ashwi@ASHWIN>
2025-10-06 10:54:00 +0530

    commit

---
+++ core_java123/src/com/tnsif/day1/IdetifiersDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.day1;
+
+public class IdetifiersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Ashwin"; //use camelcase
+		System.out.println("value is : "+ studentName);
+	}
+
+}
